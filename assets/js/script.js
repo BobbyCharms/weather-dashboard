58b8220b9cdf8e20e76229fbfa1069f4c790bc8a
BobbyCharms <robertcharming@Gmail.com>
2023-02-21 21:10:57 -0500

    first commit with basic html layout and styled css header

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let apiKey = "2bfbf3e42891f2b88fb6fb6c098571f6";
+let coordinates;
+var queryURL = ""
+
+
+fetch(queryURL)
+
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
